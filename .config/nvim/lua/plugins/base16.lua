8bc0b1af1fbf88b41f28d65c13b92a81c8ffae45
Matt Davis <mattdavis90@googlemail.com>
2023-06-04 11:42:31 +0100

    Remove redundant config

--- .config/nvim/lua/plugins/base16.lua
+++
@@ -1,5 +0,0 @@
-local base16_project_theme = os.getenv('BASE16_THEME')
-
-if base16_project_theme and vim.g.colors_name ~= 'base16-' .. base16_project_theme then
-    vim.cmd('colorscheme base16-' .. base16_project_theme)
-end
--- .config/nvim/lua/plugins/cmp-dictionary.lua
+++
@@ -1,6 +0,0 @@
-require("cmp_dictionary").setup({
-    dic = {
-        ["*"] = { "/usr/share/dict/words" },
-    },
-    first_case_insensitive = true,
-})
