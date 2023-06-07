b797c81a292e1cd203458d6cc850dcdfdcc134a1
Matt Davis <mattdavis90@googlemail.com>
2023-06-07 22:42:38 +0100

    Add bufferline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -45,7 +45,7 @@ return {
         end
     },
     {
-        'kyazdani42/nvim-web-devicons',
+        'nvim-tree/nvim-web-devicons',
         lazy = true,
     },
     {
@@ -163,4 +163,29 @@ return {
             alpha.setup(dashboard.opts)
         end
     },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        opts = {
+            options = {
+                -- close_command = function(n) require("mini.bufremove").delete(n, false) end,
+                -- right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+                diagnostics = "nvim_lsp",
+                always_show_bufferline = false,
+                diagnostics_indicator = function(count, level)
+                    local icon = level:match("error") and " " or " "
+                    return " " .. icon .. count
+                end,
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = "NvimTree",
+                        highlight = "Directory",
+                        text_align = "left",
+                    },
+                },
+            },
+        },
+    }
 }
