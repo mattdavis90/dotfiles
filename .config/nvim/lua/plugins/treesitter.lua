5fa7ff6150b0c849ffea3d1a76f508df43cc58da
Matt Davis <mattdavis90@googlemail.com>
2023-10-09 21:18:45 +0100

    Update to latest blankline

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -28,22 +28,14 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPost", "BufNewFile" },
+        main = "ibl",
         opts = {
-            char = "│",
-            filetype_exclude = {
-                "help",
-                "alpha",
-                "dashboard",
-                "neo-tree",
-                "Trouble",
-                "lazy",
-                "mason",
-                "notify",
-                "toggleterm",
-                "lazyterm",
+            indent = {
+                char = "│",
             },
-            show_trailing_blankline_indent = false,
-            show_current_context = false,
+            scope = {
+                enabled = false,
+            },
         },
     },
     {
