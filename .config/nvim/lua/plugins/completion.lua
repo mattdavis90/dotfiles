a263025aeaddcb9e110af8acbb4fd40ad16e47ce
Matt Davis <mattdavis90@googlemail.com>
2024-05-05 08:25:04 +0100

    Drop buggy snippets

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -11,7 +11,6 @@ return {
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'ray-x/cmp-treesitter',
-            'rafamadriz/friendly-snippets',
             'onsails/lspkind-nvim',
         },
         config = function()
